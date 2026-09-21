d83144203486575a1fdc20abd87479e96d9769b2
hamadqudus <hamadqudus@gmail.com>
2026-09-21 13:15:44 +0500

    Exercise 1.2: add HTTP server to log-output

--- log-output/app.js
+++ log-output/app.js
@@ -1,9 +1,18 @@
+const http = require("http");
 const crypto = require("crypto");
 
 const randomString = crypto.randomBytes(16).toString("hex");
 
-console.log(`Random string: ${randomString}`);
-
 setInterval(() => {
   console.log(`${new Date().toISOString()}: ${randomString}`);
 }, 5000);
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end(`<html><body><p>${randomString}</p></body></html>`);
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
